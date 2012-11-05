3081f0f433ea666fc4551912aafea57f857607a2
Kameron Weaver <kaweaver@mines.edu>
2012-11-05 16:50:26 -0700

    added that database and made the list of restaurants store in the db instead

--- LunchList_apt3/src/apt/tutorial/lunchlist_apt3/LunchList.java
+++ LunchList_apt3/src/apt/tutorial/lunchlist_apt3/LunchList.java
@@ -1,108 +1,36 @@
 package apt.tutorial.lunchlist_apt3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.app.TabActivity;
+import android.content.Context;
+import android.database.Cursor;
 import android.widget.TabHost;
 import android.widget.AdapterView;
-import android.view.MenuItem;
-import android.widget.Toast;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @SuppressWarnings("deprecation")
 public class LunchList extends TabActivity {
-	  List<Restaurant> model=new ArrayList<Restaurant>();
+	  Cursor model=null;
 	  RestaurantAdapter adapter=null;
 	  EditText name=null;
 	  EditText address=null;
 	  RadioGroup types=null;
 	  EditText notes=null;
 	  Restaurant current=null;
-	  int progress=0;
-	  AtomicBoolean isActive = new AtomicBoolean(true);
-	  
-	  @Override
-	  public boolean onOptionsItemSelected(MenuItem item) {
-		  if (item.getItemId()==R.id.toast) {
-			  String message="No restaurant selected";
-			  if (current!=null) {
-				  message=current.getNotes();
-			  }
-			  Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-			  return(true);
-		  }else if(item.getItemId() == R.id.run){
-			  startWork();
-			  return(true);
-		  }
-		  return(super.onOptionsItemSelected(item));
-	  }
-	  
-	  private void doSomeLongWork(final int incr) {
-		  runOnUiThread(new Runnable(){
-			  public void run(){
-				  progress+=incr;
-				  setProgress(progress);
-			  }
-		  });
-		  SystemClock.sleep(250); // should be something more useful!
-	  }
-	  
-	  private void startWork() {
-		  setProgressBarVisibility(true);
-		  new Thread(longTask).start();
-	  }
-	  
-	  @Override
-	  public void onResume() {
-		  super.onResume();
-		  isActive.set(true);
-		  if (progress>0) {
-			  startWork();
-		  }
-	  }
-	  
-	  @Override
-	  public void onPause() {
-		  super.onPause();
-		  isActive.set(false);
-	  }
-	  
-	  private Runnable longTask=new Runnable() {
-		  public void run() {
-			  for (int i=progress; i<10000 && isActive.get(); i+=200) {
-				  doSomeLongWork(200);
-			  }
-			  if (isActive.get()) {
-				  runOnUiThread(new Runnable() {
-				  public void run() {
-				  		setProgressBarVisibility(false);
-				  		progress=0;
-			  		}
-			  	});
-			  }
-		  }
-	  };
+	  RestaurantHelper helper=null;
 	  
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_PROGRESS);
 		setContentView(R.layout.activity_lunch_list);
 		
 		name=(EditText)findViewById(R.id.name);
@@ -113,7 +41,6 @@ public class LunchList extends TabActivity {
 		Button save=(Button)findViewById(R.id.save);
 		save.setOnClickListener(onSave);
 		ListView list=(ListView)findViewById(R.id.restaurants);
-		adapter=new RestaurantAdapter();
 		list.setAdapter(adapter);
 		TabHost.TabSpec spec=getTabHost().newTabSpec("tag1");
 		spec.setContent(R.id.restaurants);
@@ -125,93 +52,90 @@ public class LunchList extends TabActivity {
 		getTabHost().addTab(spec);
 		getTabHost().setCurrentTab(0);
 		list.setOnItemClickListener(onListClick);
+		helper=new RestaurantHelper(this);
+		model=helper.getAll();
+		startManagingCursor(model);
+		adapter=new RestaurantAdapter(model);
+		list.setAdapter(adapter);
 	}
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-		return(super.onCreateOptionsMenu(menu));
+	public void onDestroy() {
+		super.onDestroy();
+		helper.close();
 	}
 	
 	private View.OnClickListener onSave=new View.OnClickListener() {
-	    public void onClick(View v) {
-	      current = new Restaurant();
-	      EditText name=(EditText)findViewById(R.id.name);
-	      EditText address=(EditText)findViewById(R.id.addr);
-	      EditText notes=(EditText)findViewById(R.id.notes);
-	      
-	      current.setName(name.getText().toString());
-	      current.setAddress(address.getText().toString());
-	      current.setNotes(notes.getText().toString());
-	      RadioGroup types=(RadioGroup)findViewById(R.id.types);
-	      switch (types.getCheckedRadioButtonId()) {
-		      case R.id.sit_down:
-		    	  current.setType("sit_down");
-		    	  break;
-		      case R.id.take_out:
-		    	  current.setType("take_out");
-		    	  break;
-		      case R.id.delivery:
-		    	  current.setType("delivery");
-		    	  break;
-	      }
-	      adapter.add(current);
-	    }
-	  };
-	  class RestaurantAdapter extends ArrayAdapter<Restaurant> {
-		  RestaurantAdapter() {
-			  super(LunchList.this, android.R.layout.simple_list_item_1, model);
-		  }
-		  public View getView(int position, View convertView, ViewGroup parent) {
-			  View row=convertView;
-			  RestaurantHolder holder=null;
-			  if (row==null) {
-				  LayoutInflater inflater=getLayoutInflater();
-				  row=inflater.inflate(R.layout.row, parent, false);
-				  holder=new RestaurantHolder(row);
-				  row.setTag(holder);
-			  }else {
-				  holder=(RestaurantHolder)row.getTag();
-			  }
-			  holder.populateFrom(model.get(position));
-			  return(row);
-		  }
-	  }
-	  static class RestaurantHolder {
-		  private TextView name=null;
-		  private TextView address=null;
-		  private ImageView icon=null;
-		  RestaurantHolder(View row) {
-			  name=(TextView)row.findViewById(R.id.title);
-			  address=(TextView)row.findViewById(R.id.address);
-			  icon=(ImageView)row.findViewById(R.id.icon);
-		  }
-		  void populateFrom(Restaurant r) {
-			  name.setText(r.getName());
-			  address.setText(r.getAddress());
-			  if (r.getType().equals("sit_down")) {
-				  icon.setImageResource(R.drawable.ball_red);
-			  }else if (r.getType().equals("take_out")) {
-				  icon.setImageResource(R.drawable.ball_yellow);
-			  }else {
-				  icon.setImageResource(R.drawable.ball_green);
-			  }
-		  }
-	  }
-	  private AdapterView.OnItemClickListener onListClick=new AdapterView.OnItemClickListener() {
-		  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			  current=model.get(position);
-			  name.setText(current.getName());
-			  address.setText(current.getAddress());
-			  notes.setText(current.getNotes());
-			  if (current.getType().equals("sit_down")) {
-				  types.check(R.id.sit_down);
-			  }else if (current.getType().equals("take_out")) {
-				  types.check(R.id.take_out);
-			  }else {
-				  types.check(R.id.delivery);
-			  }
-			  getTabHost().setCurrentTab(1);
-		  }
-	  };
+		public void onClick(View v) {
+			String type=null;
+			switch (types.getCheckedRadioButtonId()) {
+				case R.id.sit_down:
+					type="sit_down";
+					break;
+				case R.id.take_out:
+					type="take_out";
+					break;
+				case R.id.delivery:
+					type="delivery";
+					break;
+			}
+			helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+			model.requery();
+		}
+	};
+	class RestaurantAdapter extends CursorAdapter {
+		RestaurantAdapter(Cursor c) {
+				super(LunchList.this, c);
+		}
+		@Override
+		public void bindView(View row, Context ctxt, Cursor c) {
+			RestaurantHolder holder=(RestaurantHolder)row.getTag();
+			holder.populateFrom(c, helper);
+		}
+		@Override
+		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
+			LayoutInflater inflater=getLayoutInflater();
+			View row=inflater.inflate(R.layout.row, parent, false);
+			RestaurantHolder holder=new RestaurantHolder(row);
+			row.setTag(holder);
+			return(row);
+		}
+	}
+	static class RestaurantHolder {
+		private TextView name=null;
+		private TextView address=null;
+		private ImageView icon=null;
+		RestaurantHolder(View row) {
+			name=(TextView)row.findViewById(R.id.title);
+			address=(TextView)row.findViewById(R.id.address);
+			icon=(ImageView)row.findViewById(R.id.icon);
+		}
+		void populateFrom(Cursor c, RestaurantHelper helper) {
+			name.setText(helper.getName(c));
+			address.setText(helper.getAddress(c));
+			if (helper.getType(c).equals("sit_down")) {
+				icon.setImageResource(R.drawable.ball_red);
+			}else if (helper.getType(c).equals("take_out")) {
+				icon.setImageResource(R.drawable.ball_yellow);
+			}else {
+				icon.setImageResource(R.drawable.ball_green);
+			}
+		}
+	}
+	private AdapterView.OnItemClickListener onListClick=new AdapterView.OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			model.moveToPosition(position);
+			name.setText(helper.getName(model));
+			address.setText(helper.getAddress(model));
+			notes.setText(helper.getNotes(model));
+			if (helper.getType(model).equals("sit_down")) {
+				types.check(R.id.sit_down);
+			}else if (helper.getType(model).equals("take_out")) {
+				types.check(R.id.take_out);
+			}else {
+				types.check(R.id.delivery);
+			}
+			getTabHost().setCurrentTab(1);
+		}
+	};
 }
