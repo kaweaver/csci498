918ea790649422e37842a946d4d3e76ab540cc88
Kameron Weaver <kaweaver@mines.edu>
2012-11-05 16:57:34 -0700

    added the feed url to the db and the menu item for the user when viewing a restaurant

--- LunchList_apt3/src/apt/tutorial/lunchlist_apt3/DetailForm.java
+++ LunchList_apt3/src/apt/tutorial/lunchlist_apt3/DetailForm.java
@@ -2,13 +2,20 @@ package apt.tutorial.lunchlist_apt3;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -19,6 +26,7 @@ public class DetailForm extends Activity {
 	EditText name=null;
 	EditText address=null;
 	EditText notes=null;
+	EditText feed=null;
 	RadioGroup types=null;
 	RestaurantHelper helper=null;
 	String restaurantId=null;
@@ -31,6 +39,7 @@ public class DetailForm extends Activity {
 		address=(EditText)findViewById(R.id.addr);
 		notes=(EditText)findViewById(R.id.notes);
 		types=(RadioGroup)findViewById(R.id.types);
+		feed=(EditText)findViewById(R.id.feed);
 		Button save=(Button)findViewById(R.id.save);
 		save.setOnClickListener(onSave);
 		restaurantId=getIntent().getStringExtra(LunchList.ID_EXTRA);
@@ -44,6 +53,7 @@ public class DetailForm extends Activity {
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
 		}else if (helper.getType(c).equals("take_out")) {
@@ -76,11 +86,13 @@ public class DetailForm extends Activity {
 			if (restaurantId==null) {
 				helper.insert(name.getText().toString(),
 				address.getText().toString(), type,
-				notes.getText().toString());
+				notes.getText().toString(),
+				feed.getText().toString());
 			}else {
 				helper.update(restaurantId, name.getText().toString(),
 				address.getText().toString(), type,
-				notes.getText().toString());
+				notes.getText().toString(),
+				feed.getText().toString());
 			}
 			finish();
 		}
@@ -101,5 +113,32 @@ public class DetailForm extends Activity {
 		notes.setText(state.getString("notes"));
 		types.check(state.getInt("type"));
 	}
-	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.details_option, menu);
+		return(super.onCreateOptionsMenu(menu));
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()==R.id.feed) {
+			if (isNetworkAvailable()) {
+				Intent i=new Intent(this, FeedActivity.class);
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			}else {
+				Toast
+				.makeText(this, "Sorry, the Internet is not available",
+				Toast.LENGTH_LONG)
+				.show();
+			}
+			return(true);
+		}
+		return(super.onOptionsItemSelected(item));
+	}
+	private boolean isNetworkAvailable() {
+		ConnectivityManager
+		cm=(ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info=cm.getActiveNetworkInfo();
+		return(info!=null);
+	}
 }
