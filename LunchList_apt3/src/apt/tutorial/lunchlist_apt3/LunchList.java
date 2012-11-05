dd3d7fceaada0198285b3444d7a47ab6acf3f783
Kameron Weaver <kaweaver@mines.edu>
2012-11-05 16:39:31 -0700

    finished chapter 4 of the tutorial

--- LunchList_apt3/src/apt/tutorial/lunchlist_apt3/LunchList.java
+++ LunchList_apt3/src/apt/tutorial/lunchlist_apt3/LunchList.java
@@ -1,25 +1,36 @@
 package apt.tutorial.lunchlist_apt3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
-	  Restaurant r=new Restaurant();
+	  List<Restaurant> model=new ArrayList<Restaurant>();
+	  ArrayAdapter<Restaurant> adapter=null;
 	  
 	  @Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_lunch_list);
-	Button save=(Button)findViewById(R.id.save);
+	    Button save=(Button)findViewById(R.id.save);
 	    
 	    save.setOnClickListener(onSave);
+	    
+	    ListView list=(ListView)findViewById(R.id.restaurants);
+	    adapter=new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
+	    list.setAdapter(adapter);
 	  }
 	private View.OnClickListener onSave=new View.OnClickListener() {
 	    public void onClick(View v) {
+	      Restaurant r = new Restaurant();
 	      EditText name=(EditText)findViewById(R.id.name);
 	      EditText address=(EditText)findViewById(R.id.addr);
 	      
@@ -27,16 +38,17 @@ public class LunchList extends Activity {
 	      r.setAddress(address.getText().toString());
 	      RadioGroup types=(RadioGroup)findViewById(R.id.types);
 	      switch (types.getCheckedRadioButtonId()) {
-		      case R.id.sitdown:
+		      case R.id.sit_down:
 		    	  r.setType("sit_down");
 		    	  break;
-		      case R.id.takeout:
+		      case R.id.take_out:
 		    	  r.setType("take_out");
 		    	  break;
 		      case R.id.delivery:
 		    	  r.setType("delivery");
 		    	  break;
 	      }
+	      adapter.add(r);
 	    }
 	  };
 }
