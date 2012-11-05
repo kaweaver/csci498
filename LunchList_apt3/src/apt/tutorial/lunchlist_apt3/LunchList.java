fcf4070ed6430e4ac87876a49fbf8bc81d11cc05
Kameron Weaver <kaweaver@mines.edu>
2012-11-05 16:38:24 -0700

    finished the third chapter of the tutorial

---
+++ LunchList_apt3/src/apt/tutorial/lunchlist_apt3/LunchList.java
@@ -0,0 +1,42 @@
+package apt.tutorial.lunchlist_apt3;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class LunchList extends Activity {
+	  Restaurant r=new Restaurant();
+	  
+	  @Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_lunch_list);
+	Button save=(Button)findViewById(R.id.save);
+	    
+	    save.setOnClickListener(onSave);
+	  }
+	private View.OnClickListener onSave=new View.OnClickListener() {
+	    public void onClick(View v) {
+	      EditText name=(EditText)findViewById(R.id.name);
+	      EditText address=(EditText)findViewById(R.id.addr);
+	      
+	      r.setName(name.getText().toString());
+	      r.setAddress(address.getText().toString());
+	      RadioGroup types=(RadioGroup)findViewById(R.id.types);
+	      switch (types.getCheckedRadioButtonId()) {
+		      case R.id.sitdown:
+		    	  r.setType("sit_down");
+		    	  break;
+		      case R.id.takeout:
+		    	  r.setType("take_out");
+		    	  break;
+		      case R.id.delivery:
+		    	  r.setType("delivery");
+		    	  break;
+	      }
+	    }
+	  };
+}
---
+++ LunchList_apt3/src/apt/tutorial/lunchlist_apt3/Restaurant.java
@@ -0,0 +1,26 @@
+package apt.tutorial.lunchlist_apt3;
+
+public class Restaurant {
+	private String name="";
+	private String address="";
+	private String type="";
+	
+	public String getType(){
+		return(type);
+	}
+	public void setType(String type){
+		this.type=type;
+	}
+	public String getName() {
+	    return(name);
+	  }
+	public void setName(String name) {
+	    this.name=name;
+	  }
+	public String getAddress() {
+	    return(address);
+	  }
+	public void setAddress(String address) {
+	    this.address=address;
+	  }
+}
